b12e75363759de66141aee9f3b0bec1efe4c5c69
prashxant <notxdead0@gmail.com>
2025-05-02 18:53:31 +0530

    yt embedded

--- brainly-fr/src/App.tsx
+++ brainly-fr/src/App.tsx
@@ -1,6 +1,7 @@
 import {  Button } from "./comonents/Button"
 import { PlusIcon } from "./icons/PlusIcon"
 import { Share } from "./icons/Share"
+import { Card } from "./comonents/Card"
 function App() {
 
   return (
@@ -8,6 +9,8 @@ function App() {
     <div className="">
       <Button variant="primary" text="Add contents" startIcon={<PlusIcon/>}/>
       <Button variant="secondary" text="Share Brain" startIcon={<Share/>}/>
+      <br />
+      <Card/>
     </div>
    </>
   )
--- brainly-fr/src/comonents/Card.tsx
+++ brainly-fr/src/comonents/Card.tsx
@@ -1,10 +1,32 @@
+import { Share } from "../icons/Share";
 
-
-export function Card () {
-
-    return(
-        <div className="bg-white rounded-md ">
-
+export function Card() {
+  return (
+    <div>
+      <div className="p-4 bg-white rounded-md  border-gray-200 max-w-72    border">
+        <div className="flex justify-between ">
+          <div className="flex items-center text-md">
+            <div className="text-gray-500 pr-2">
+              <Share />
+            </div>
+            <div>Project ideas</div>
+          </div>
+          <div className="flex items-center">
+            <div className="text-gray-500 pr-2">
+              <Share />
+            </div>
+            <div className="text-gray-500 pr-2">
+              <Share />
+            </div>
+          </div>
         </div>
-    )
+        <br />
+        <iframe className="w-full" src="https://www.youtube.com/embed/gXQrci3Wff8?si=nVxsnYsZnMsEokax" 
+               title="YouTube video player"  allow="accelerometer;autoplay; 
+               clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        
+      </div>
+    </div>
+  );
 }
