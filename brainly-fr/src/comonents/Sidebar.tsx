d753e0e1b33956bf3c999ece6187abbd5e4d6e4d
prashxant <notxdead0@gmail.com>
2025-05-06 19:49:26 +0530

    NCC

--- brainly-fr/src/comonents/Sidebar.tsx
+++ brainly-fr/src/comonents/Sidebar.tsx
@@ -2,6 +2,6 @@
 export function Sidebar (){
 
     return <div className="h-screen bg-white border-r w-72 fixed left-0 top-0 rounded-md">
-            USELESSCOMMIT
+            woahhh
     </div>
 }
