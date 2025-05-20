05da59e8701ede2d1cdada6f7950a8805376882e
prashxant <notxdead0@gmail.com>
2025-05-20 21:59:07 +0530

    academic logo

--- brainly-fr/src/comonents/Sidebar.tsx
+++ brainly-fr/src/comonents/Sidebar.tsx
@@ -1,11 +1,17 @@
+import { AcademicIcon } from "../icons/AcademicIcon";
 import { TwitterIcon } from "../icons/TwitterIcon";
 import { YoutubeIcon } from "../icons/YoutubeIcon";
 import { SidebarComponent } from "./SidebarComponent";
 
 export function Sidebar (){
 
-    return <div className="h-screen bg-white border-r w-72 fixed left-0 top-0 rounded-md">
-            <div className="pt-4 items  ">
+    return <div className="h-screen bg-white border-r w-72 fixed left-0 top-0 rounded-md pl-6">
+
+                <div className="flex text-2xl pt-4">
+                   <AcademicIcon/> Brainly
+                </div>
+            <div className="pt-4">
+                
                 <SidebarComponent text="Twitter" icon={<TwitterIcon/>} />
                 <SidebarComponent text="Youtube" icon={<YoutubeIcon/>} />
             </div>
--- brainly-fr/src/comonents/SidebarComponent.tsx
+++ brainly-fr/src/comonents/SidebarComponent.tsx
@@ -6,11 +6,11 @@ export function SidebarComponent({text,icon}:{
     text: string;
     icon: ReactElement;
 }) {
-  return <div className="flex text-gray-700 items-center ">
-    <div className="p-2 pl-6">
+  return <div className="flex text-gray-700 items-center py-2 ">
+    <div className="pr-2">
       {icon}
     </div>
-    <div className="p-2 pb-3">
+    <div className="">
     {text}
     </div>
   </div>;
