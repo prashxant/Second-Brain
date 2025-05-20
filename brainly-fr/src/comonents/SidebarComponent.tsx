a1f9b915b6d26a346b26551ce0ce48466c1c68c7
prashxant <notxdead0@gmail.com>
2025-05-20 22:25:33 +0530

    pages folder , clear cluttering

--- brainly-fr/src/comonents/SidebarComponent.tsx
+++ brainly-fr/src/comonents/SidebarComponent.tsx
@@ -6,7 +6,8 @@ export function SidebarComponent({text,icon}:{
     text: string;
     icon: ReactElement;
 }) {
-  return <div className="flex text-gray-700 items-center py-2 px-4 cursor-pointer hover:bg-purple-200 rounded-md">
+  return <div className="flex text-gray-700 items-center py-2 px-4 cursor-pointer
+   hover:bg-purple-200 rounded-md max-w-48">
     <div className="pr-2">
       {icon}
     </div>
